57803b279156b81c5c041350c0d23203e953a0ba
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-05-15 16:07:05 +0900

    JSON.parse

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 'use strict';
 
 module.exports.handler = (event, context, callback) => {
+    console.log(event);
+
+    if (event.httpMethod === 'POST') {
+        var body = JSON.parse(event.body);
+
+        if (body.repository && body.repository) {
+            var namespace = body.repository.namespace;
+            var name = body.repository.name;
+            var tag = body.push_data.tag;
+
+            console.log('namespace : ' + namespace);
+            console.log('name : ' + name);
+            console.log('tag : ' + tag);
+        }
+    }
+
     const res = {
         statusCode: 200,
         headers: {
             'Access-Control-Allow-Origin': '*', // Required for CORS support to work
         },
-        body: JSON.stringify({
-            message: 'OK',
-            input: event,
-        }),
+        body: 'OK'
     };
-    console.log('event : ', event);
     callback(null, res);
 };
